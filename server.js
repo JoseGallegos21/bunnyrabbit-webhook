a85ab3af62562dc8238c3683b121774da3e2494f
JoseGallegos21 <dark.jg93@gmail.com>
2026-05-19 15:32:19 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const VERIFY_TOKEN = 'bunnyrabbit4905';
+
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    console.log('Webhook verificado correctamente');
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook', (req, res) => {
+  console.log('Evento recibido:', JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Servidor corriendo en puerto ' + PORT));
